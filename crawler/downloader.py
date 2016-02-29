ad771a5fbb1de71ae896c40dd4be6da53e000bd4
Frank Luan <franklsf95@gmail.com>
2016-02-29 00:26:14 -0600

    Downloader now runs multi-threaded

--- crawler/downloader.py
+++ crawler/downloader.py
@@ -4,7 +4,9 @@
 import ast
 import logging
 import os
+import queue
 import requests
+import threading
 
 # Set up logging
 
@@ -50,30 +52,60 @@ def download(url, file_path, overwrite=False):
         log.info("Successfully saved {}.".format(url))
 
 
-def download_all(results, offset=0):
+task_queue = queue.Queue()
+
+
+def thread_worker():
+    global task_queue
+    while True:
+        item = task_queue.get()
+        if item is None:
+            return
+        url, file_path = item
+        download(url, file_path)
+        task_queue.task_done()
+
+
+def download_all(results, offset=0, n_threads=10):
     """
     Download all results to local files
     :param results: list of triplets
     :param offset: int
+    :param n_threads: int
     :return: None
     """
-    root_path = '../tmp/profiles/'
+    root_path = '../tmp/banks/'
     if not os.path.exists(root_path):
         os.makedirs(root_path)
 
+    global task_queue
     for triplet in results[offset:]:
         if triplet is None:
             continue
         conf, title, url = triplet
         file_path = os.path.join(root_path, make_filename(title))
-        download(url, file_path)
+        task_queue.put((url, file_path))
+
+    threads = []
+    for i in range(n_threads):
+        t = threading.Thread(target=thread_worker)
+        t.start()
+        threads.append(t)
+
+    task_queue.join()
+    log.info('All tasks done. Stopping.')
+
+    for i in range(n_threads):
+        task_queue.put(None)
+    for t in threads:
+        t.join()
 
 
 def main():
     """
     Driver method.
     """
-    file_path = '../tmp/html_urls.py'
+    file_path = '../tmp/bank_links.py'
     with open(file_path) as file:
         content = file.read()
         people = ast.literal_eval(content)
